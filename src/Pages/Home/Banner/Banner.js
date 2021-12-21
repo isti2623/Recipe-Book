5eb6ae9096286bef3e509195ed75891cd7e8058a
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-12-21 12:43:23 +0600

    update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Banner.css'
 import banner from '../../../images/banner.jpg';
 
+
 const Banner = () => {
+
     return (
 
         <div>
 
             <img src={banner} alt="" className='opacity-100 w-100 rounded-3 p-5' />
-
 
         </div>
 
